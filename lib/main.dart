1058037eea1591d9dfe7b5840f148d2837d6cad6
Lenah_Mbatha <lenahmbatha5@gmail.com>
2024-03-19 11:06:41 +0000

    myCommitToday

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,112 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
-void main() => runApp(MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
+      title: 'Firebase Auth',
       theme: ThemeData(
-        // useMaterial3: false,
         primarySwatch: Colors.blue,
       ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: FutureBuilder(
+        future: _initialization,
+        builder: (BuildContext context, AsyncSnapshot<FirebaseApp> snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const CircularProgressIndicator();
+          } else {
+            if (snapshot.hasError) {
+              return const Text('Error initializing Firebase');
+            } else {
+              if (_auth.currentUser != null) {
+                return HomePage(user: _auth.currentUser!);
+              } else {
+                return SignInPage();
+              }
+            }
+          }
+        },
+      ),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
+class SignInPage extends StatelessWidget {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
+        title: const Text('Sign In'),
       ),
       body: Center(
-        child: Text(
-          'Hello, World!',
+        child: ElevatedButton(
+          child: const Text('Sign In'),
+          onPressed: () async {
+            UserCredential? userCredential;
+            try {
+              userCredential = await _auth.signInWithEmailAndPassword(
+                email: 'example@example.com',
+                password: 'password',
+              );
+            } catch (e) {
+              print(e.toString());
+            }
+
+            if (userCredential != null) {
+              Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => HomePage(user: userCredential!.user!),
+                ),
+              );
+            }
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  final User user;
+
+  const HomePage({Key? key, required this.user}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Welcome, ${user.email}'),
+            const SizedBox(height: 16.0),
+            ElevatedButton(
+              child: const Text('Sign Out'),
+              onPressed: () async {
+                await FirebaseAuth.instance.signOut();
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(builder: (context) => SignInPage()),
+                );
+              },
+            ),
+          ],
         ),
       ),
     );
